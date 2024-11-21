8e927e353b80ae08c38f3320e1d93a32343deb13
Justintime <159927798+JustinasKelm@users.noreply.github.com>
2024-11-21 21:54:34 +0200

    Updated Zalgirio delikatesas.py
    added way to select teams that are playing
    added a way to know which teams are playing
    added 2 teams
    refined 2 teams
    fixed bug with foul percentages

--- Zalgirio delikatesas.py
+++ Zalgirio delikatesas.py
@@ -28,108 +28,116 @@ class team:
         self.trnv=trnv
         self.ftp=ftp
         self.flp=flp
-zal = team(454,346,60,334,191,685,151)
-pan = team(529,422,100,301,177,715,301)
 
-zalx = team(1000,1000,100,1000,10,1000,1000)
-panx = team(1000,1000,100,1000,10,1000,1000)
+teamchoice1=str(input("Įveskite pirmąją komandą: "))
+if teamchoice1 == "ZAL":t1 = team(504,314,62,151,145,692,288) #r10
+elif teamchoice1 == "PAN":t1 = team(593,414,132,126,139,704,213) #r10
+elif teamchoice1 == "BAS":t1 = team(501,307,120,162,133,812,213) #r10
+elif teamchoice1 == "PAR":t1 = team(518,373,188,132,139,776,258) #r10
+teamchoice2=str(input("Įveskite antrąją komandą: "))
+if teamchoice2 == "ZAL":t2 = team(504,314,62,151,145,685,288)
+elif teamchoice2 == "PAN":t2 = team(593,414,132,126,139,704,213)
+elif teamchoice2 == "BAS":t2 = team(501,307,120,162,133,812,213)
+elif teamchoice2 == "PAR":t2 = team(518,373,188,132,139,776,258) 
 
+print()
+print(teamchoice1,teamchoice2)
 for i in range(qrt):
     for j in range(poss):
         if random.randint(1,10)<4:
-            if random.randint(1,1000)>zal.trnv:
-                if random.randint(1,1100-pan.d)<zal.o3:
+            if random.randint(1,1000)>t1.trnv:
+                if random.randint(1,1100-t2.d)<t1.o3:
                     tc1+=3
                     list1.append(3)
                 if tc1>0:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t2.flp:
                         fc1+=1
-                        if random.randint(1,1000)<zal.ftp:
+                        if random.randint(1,1000)<t1.ftp:
                             tc1+=1
                             ftpc1+=1
                 else:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t2.flp:
                         fc1+=1
                         for i in range(3):
-                            if random.randint(1,1000)<zal.ftp:
+                            if random.randint(1,1000)<t1.ftp:
                                 tc1+=1
                                 ftpc1+=1
             else:
                 trnvc1+=1
                 tvc1+=1
         if random.randint(1,10)<4:
-            if random.randint(1,1000)>pan.trnv: 
-                if random.randint(1,1100-zal.d)<pan.o3:
+            if random.randint(1,1000)>t2.trnv: 
+                if random.randint(1,1100-t1.d)<t2.o3:
                     tc2+=3
                     list2.append(3)
                 if tc2>0:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t1.flp:
                         fc2+=1
-                        if random.randint(1,1000)<zal.ftp:
+                        if random.randint(1,1000)<t2.ftp:
                             tc2+=1
                             ftpc2+=1
                 else:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t1.flp:
                         fc2+=1
                         for i in range(3):
-                            if random.randint(1,1000)<zal.ftp:
+                            if random.randint(1,1000)<t2.ftp:
                                 tc2+=1
                                 ftpc2+=1
             else:
                 trnvc2+=1
                 tvc2+=1
         if tc1==0 and tvc1==0:
-            if random.randint(1,1000)>zal.trnv:
-                if random.randint(1,1100-pan.d)<zal.o2:
+            if random.randint(1,1000)>t1.trnv:
+                if random.randint(1,1100-t2.d)<t1.o2:
                     tc1+=2
                     list1.append(2)
                 if tc1>0:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t2.flp:
                         fc1+=1
-                        if random.randint(1,1000)<zal.ftp:
+                        if random.randint(1,1000)<t1.ftp:
                             tc1+=1
                             ftpc1+=1
                 else:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t2.flp:
                         fc1+=1
                         for i in range(2):
-                            if random.randint(1,1000)<zal.ftp:
+                            if random.randint(1,1000)<t1.ftp:
                                 tc1+=1
                                 ftpc1+=1
             else:
                 trnvc1+=1
         if tc2==0 and tvc2==0:
-            if random.randint(1,1000)>pan.trnv: 
-                if random.randint(1,1100-zal.d)<pan.o2:
+            if random.randint(1,1000)>t2.trnv: 
+                if random.randint(1,1100-t1.d)<t2.o2:
                     tc2+=2
                     list2.append(2)
                 if tc2>0:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t1.flp:
                         fc2+=1
-                        if random.randint(1,1000)<zal.ftp:
+                        if random.randint(1,1000)<t2.ftp:
                             tc2+=1
                             ftpc2+=1
                 else:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t1.flp:
                         fc2+=1
                         for i in range(2):
-                            if random.randint(1,1000)<zal.ftp:
+                            if random.randint(1,1000)<t2.ftp:
                                 tc2+=1
                                 ftpc2+=1
             else:
                 trnvc2+=1
         if tc1 == 0 and tvc1==0 or tc2 == 0 and tvc2==0:
-            if random.randint(1,1000)<zal.scp and tc1 == 0 and tvc1==0:
-                if random.randint(1,1000)>zal.trnv:
-                    if random.randint(1,1100-pan.d)<zal.o2:
+            if random.randint(1,1000)<t1.scp and tc1 == 0 and tvc1==0:
+                if random.randint(1,1000)>t1.trnv:
+                    if random.randint(1,1100-t2.d)<t1.o2:
                         tc1+=2
                         scpc1+=2
                         list1.append(2)
                 else:
                     trnvc1+=1    
-            if random.randint(1,1000)<pan.scp and tc2 == 0 and tvc2==0:
-                if random.randint(1,1000)>pan.trnv:
-                    if random.randint(1,1100-zal.d)<pan.o2:
+            if random.randint(1,1000)<t2.scp and tc2 == 0 and tvc2==0:
+                if random.randint(1,1000)>t2.trnv:
+                    if random.randint(1,1100-t1.d)<t2.o2:
                         tc2+=2
                         scpc2+=2
                         list2.append(2)
@@ -145,106 +153,106 @@ for i in range(qrt):
     print(f"{sum(list1)} {sum(list2)} Q{qrc}")
     time.sleep(3)
 print("")
-print(f"End of Regulation score: {sum(list1)} - {sum(list2)}  Second chance points: {scpc1} - {scpc2}  Turnovers: {trnvc1} - {trnvc2}")
+print(f"End of Regulation score: {teamchoice1} {sum(list1)} - {sum(list2)} {teamchoice2}  Second chance points: {scpc1} - {scpc2}  Turnovers: {trnvc1} - {trnvc2}")
 print(f"Foul points {ftpc1} - {ftpc2}")
 print("")
 while sum(list1) == sum(list2):
     for i in range(poss//2):
         if random.randint(1,10)<4:
-            if random.randint(1,1000)>zal.trnv:
-                if random.randint(1,1100-pan.d)<zal.o3:
+            if random.randint(1,1000)>t1.trnv:
+                if random.randint(1,1100-t2.d)<t1.o3:
                     tc1+=3
                     list1.append(3)
                 if tc1>0:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t2.flp:
                         fc1+=1
-                        if random.randint(1,1000)<zal.ftp:
+                        if random.randint(1,1000)<t1.ftp:
                             tc1+=1
                             ftpc1+=1
                 else:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t2.flp:
                         fc1+=1
                         for i in range(3):
-                            if random.randint(1,1000)<zal.ftp:
+                            if random.randint(1,1000)<t1.ftp:
                                 tc1+=1
                                 ftpc1+=1
             else:
                 trnvc1+=1
                 tvc1+=1
         if random.randint(1,10)<4:
-            if random.randint(1,1000)>pan.trnv: 
-                if random.randint(1,1100-zal.d)<pan.o3:
+            if random.randint(1,1000)>t2.trnv: 
+                if random.randint(1,1100-t1.d)<t2.o3:
                     tc2+=3
                     list2.append(3)
                 if tc2>0:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t1.flp:
                         fc2+=1
-                        if random.randint(1,1000)<zal.ftp:
+                        if random.randint(1,1000)<t2.ftp:
                             tc2+=1
                             ftpc2+=1
                 else:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t1.flp:
                         fc2+=1
                         for i in range(3):
-                            if random.randint(1,1000)<zal.ftp:
+                            if random.randint(1,1000)<t2.ftp:
                                 tc2+=1
                                 ftpc2+=1
             else:
                 trnvc2+=1
                 tvc2+=1
         if tc1==0 and tvc1==0:
-            if random.randint(1,1000)>zal.trnv:
-                if random.randint(1,1100-pan.d)<zal.o2:
+            if random.randint(1,1000)>t1.trnv:
+                if random.randint(1,1100-t2.d)<t1.o2:
                     tc1+=2
                     list1.append(2)
                 if tc1>0:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t2.flp:
                         fc1+=1
-                        if random.randint(1,1000)<zal.ftp:
+                        if random.randint(1,1000)<t1.ftp:
                             tc1+=1
                             ftpc1+=1
                 else:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t2.flp:
                         fc1+=1
                         for i in range(2):
-                            if random.randint(1,1000)<zal.ftp:
+                            if random.randint(1,1000)<t1.ftp:
                                 tc1+=1
                                 ftpc1+=1
             else:
                 trnvc1+=1
         if tc2==0 and tvc2==0:
-            if random.randint(1,1000)>pan.trnv: 
-                if random.randint(1,1100-zal.d)<pan.o2:
+            if random.randint(1,1000)>t2.trnv: 
+                if random.randint(1,1100-t1.d)<t2.o2:
                     tc2+=2
                     list2.append(2)
                 if tc2>0:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t1.flp:
                         fc2+=1
-                        if random.randint(1,1000)<zal.ftp:
+                        if random.randint(1,1000)<t2.ftp:
                             tc2+=1
                             ftpc2+=1
                 else:
-                    if random.randint(1,1000)<zal.flp:
+                    if random.randint(1,1000)<t1.flp:
                         fc2+=1
                         for i in range(2):
-                            if random.randint(1,1000)<zal.ftp:
+                            if random.randint(1,1000)<t2.ftp:
                                 tc2+=1
                                 ftpc2+=1
             else:
                 trnvc2+=1
                 tvc2+=1
         if tc1 == 0 and tvc1==0 or tc2 == 0 and tvc2==0:
-            if random.randint(1,1000)<zal.scp and tc1 == 0:
-                if random.randint(1,1000)>zal.trnv:
-                    if random.randint(1,1100-pan.d)<zal.o2:
+            if random.randint(1,1000)<t1.scp and tc1 == 0:
+                if random.randint(1,1000)>t1.trnv:
+                    if random.randint(1,1100-t2.d)<t1.o2:
                         tc1+=2
                         scpc1+=2
                         list1.append(2)
                 else:
                     trnvc1+=1    
-            if random.randint(1,1000)<pan.scp and tc2 == 0:
-                if random.randint(1,1000)>pan.trnv:
-                    if random.randint(1,1100-zal.d)<pan.o2:
+            if random.randint(1,1000)<t2.scp and tc2 == 0:
+                if random.randint(1,1000)>t2.trnv:
+                    if random.randint(1,1100-t1.d)<t2.o2:
                         tc2+=2
                         scpc2+=2
                         list2.append(2)
@@ -260,10 +268,8 @@ while sum(list1) == sum(list2):
     otc+=1
     print(f"{sum(list1)} {sum(list2)} OT{otc}")
     time.sleep(3)
-
 if qrc>4:
     print("")
-    print(f"Final score: {sum(list1)} - {sum(list2)}  Second chance points: {scpc1} - {scpc2}")
+    print(f"Final score: {teamchoice1} {sum(list1)} - {sum(list2)} {teamchoice2}  Second chance points: {scpc1} - {scpc2}")
     print(f"Foul points {ftpc1} - {ftpc2}  Turnovers: {trnvc1} - {trnvc2}")
     print("")
-
